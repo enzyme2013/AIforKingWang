5725d1378faeaafca3bc928795dbeb8206ecfbd7
liuyj <liuyj3000@126.com>
2023-06-17 11:33:52 +0800

    train_and_validate add test result

--- catclassify/cat_classify.py
+++ catclassify/cat_classify.py
@@ -11,7 +11,8 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 # 数据准备
-data_dir = './catdata/'
+data_dir = './catdata/cat_12_train/'
+test_dir= './catdata/cat_12_test/'
 annotations_file = './catdata/train_list.txt'
 MODEL_PATH = 'catdata/cat_classification_model.pth'
 
@@ -31,54 +32,62 @@ data_transform = transforms.Compose([
     # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
-
-model = models.resnet50(pretrained=True)
+model = models.resnet50()
 
 
 # 自定义数据集类
 class CatDataset(Dataset):
-    train_loader:DataLoader
-    validate_loader:DataLoader
-    def __init__(self, data_dir, annotations_file, split=0.85, transform=None):
-        self.data_dir = data_dir
+    train_loader: DataLoader
+    validate_loader: DataLoader
+    test_loader: DataLoader
+
+    def __init__(self, is_test=False, transform=None):
         self.transform = transform
-        self.annotations = self.parse_annotations(annotations_file)
-        train_size = int(len(self) * split)
-        test_size = int(len(self) * (1 - split))
-        train_dataset, validate_data_set = torch.utils.data.random_split(self, [train_size, test_size])
-        self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        self.validate_loader = DataLoader(validate_data_set,batch_size=batch_size,shuffle=True)
+        self.is_test_mode = is_test
+        self.imgs, self.labels = self.load_dataset()
+        # self.annotations = self.parse_annotations(annotations_file)
+        # train_size = int(len(self) * split)
+        # test_size = int(len(self) * (1 - split))
+        # train_dataset, validate_data_set = torch.utils.data.random_split(self, [train_size, test_size])
+        # self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        # self.validate_loader = DataLoader(validate_data_set, batch_size=batch_size, shuffle=True)
+        # self.test_loader = self.load_test_dataset()
 
     def __len__(self):
-        return len(self.annotations)
+        return len(self.imgs)
 
-    def __getitem__(self, idx):
-        img_path, label = self.annotations[idx]
+    def __getitem__(self, index):
+        img_path = self.imgs[index]
         img = Image.open(img_path).convert('RGB')
-
         if self.transform:
             img = self.transform(img)
+        if self.is_test_mode:
+            return img, img_path
+        else:
+            return img, self.labels[index]
 
-        return img, label
-
-    def parse_annotations(self, annotations_file):
-        annotations = []
-        with open(annotations_file, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                # print(line)
-                img_path, label = line.split("\t")
-                img_path = os.path.join(self.data_dir, img_path)
-                # print(img_path)
-                label = int(label)
-                annotations.append((img_path, label))
-        return annotations
-
+    def load_dataset(self):
+        images = []
+        labels = []
+        if self.is_test_mode:
+            files = os.listdir(test_dir)
+            for img in files:
+                images.append(os.path.join(test_dir, img))
+        else:
+            with open(annotations_file, 'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    # print(line)
+                    img_path, label = line.split("\t")
+                    img_path = os.path.join(data_dir, img_path)
+                    images.append(img_path)
+                    label = int(label)
+                    labels.append(label)
+        return images, labels
 
 
 # 加载数据集
 
-dataset = CatDataset(data_dir, annotations_file, split=split_percent,transform=data_transform)
 num_features = model.fc.in_features
 model.fc = nn.Linear(num_features, num_classes)
 # 定义损失函数和优化器
@@ -90,15 +99,14 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
 
-def train(num_epochs = 10):
+def train(dataset_loader,num_epochs=10):
     # 训练模型
     losses = []
     for epoch in range(num_epochs):
         model.train()
-        for inputs, labels in dataset.train_loader:
+        for inputs, labels in dataset_loader:
             inputs = inputs.to(device)
             labels = labels.to(device)
-
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
@@ -106,14 +114,13 @@ def train(num_epochs = 10):
             optimizer.step()
         print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, loss.item()))
         losses.append(loss.item())
-
     print(losses)
     # 保存模型
     torch.save(model.state_dict(), MODEL_PATH)
 
 
-def validate():
-    model.load_state_dict(torch.load(MODEL_PATH))
+def validate(dataset_loader):
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
     model.eval()
     running_accuracy = 0
     total = 0
@@ -121,11 +128,10 @@ def validate():
     if device.type == "cuda":
         is_cuda = True
     with torch.no_grad():
-        for data in dataset.validate_loader:
+        for data in dataset_loader:
             inputs, labels = data
             if is_cuda:
                 inputs, labels = inputs.cuda(), labels.cuda()
-            # outputs = outputs.to(torch.int)
             predicted_outputs = model(inputs)
             _, predicted = torch.max(predicted_outputs, 1)
             total += labels.size(0)
@@ -133,24 +139,36 @@ def validate():
         print('Accuracy: %.2f %%' % (100 * running_accuracy / total))
 
 
-def test_pic(img_path):
-    model.load_state_dict(torch.load(MODEL_PATH))
+def train_and_validate():
+    dataset = CatDataset(is_test=False, transform=data_transform)
+    train_size = int(len(dataset) * split_percent)
+    test_size = len(dataset) - train_size
+    train_dataset, validate_data_set = torch.utils.data.random_split(dataset, [train_size, test_size])
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    validate_loader = DataLoader(validate_data_set, batch_size=batch_size, shuffle=True)
+    train(train_loader)
+    validate(validate_loader)
+
+
+def test():
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
     model.eval()
+    test_dataset = CatDataset(is_test=True, transform=data_transform)
+    dataset_loader = DataLoader(test_dataset, shuffle=False)
+    is_cuda = False
+    with torch.no_grad():
+        size = len(test_dataset)
+        # for i in range(size):
+        for data in dataset_loader:
+            inputs,image_path = data#dataset_loader[i]
+            image_path = image_path[0][10:]
+            # image_path = image_path.replace("\.\/catadata\/","")
+            if is_cuda:
+                inputs, labels = inputs.cuda(), labels.cuda()
+            predicted_outputs = model(inputs)
+            _, predicted = torch.max(predicted_outputs, 1)
+            print(f"{image_path},{predicted.item()}")
 
-    # img = np.asarray(Image.open(img_path))
-    # imgplot = plt.imshow(img)
-    # pylab.show()
-    img = data_transform(Image.open(img_path))
-    if device.type == "cuda":
-        img = img.cuda()
-    predict_output = model(img.unsqueeze(0))
-    _, predict = torch.max(predict_output,1)
-    #
 
-    print(predict)
-
-
-train(30)
-validate()
-# matplotlib.use('TkAgg')
-# test_pic("./catdata/cat_12_test/0inVXMEgaBO4Fcrhdj9bkLvHzN71yTuI.jpg")
+#train_and_validate()
+test()
